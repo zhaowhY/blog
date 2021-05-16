b6f8a373ac99d971d37e7375f91eb4f7674e50e8
zhaowenhao14 <zhaowenhao14@jd.com>
2021-05-16 22:36:01 +0800

    chore

--- interview-write/new.js
+++ interview-write/new.js
@@ -1,18 +1,18 @@
 /**
  * @description new 操作符
  * 1. 创建一个新的对象
- * 2. 将构造函数的作用域赋给新的对象
- * 3. 执行构造函数中的代码
- * 4. 返回新的对象
+ * 2. 将构造函数的作用域（包含prototype）赋给新的对象
+ * 3. 将新创建的对象作为this的上下文，执行构造函数中的代码
+ * 4. 返回该新对象;
  */
 
 
 function myNew(A, ...rest) {
-  // 1. 创建一个新的对象; 2.将构造函数的作用域赋给新的对象;
+  // es5原型继承， 1. 创建一个新的对象; 2.将构造函数的作用域（包含prototype）赋给新的对象
   const _constructor = Object.create(A.prototype);
 
-  // 3.执行构造函数中的代码;
+  // 3. 将新创建的对象作为this的上下文，执行构造函数中的代码
   const obj = A.apply(_constructor, rest);
-  // 4. 返回新的对象;
+  // 4. 返回该新对象;
   return obj;
 }
